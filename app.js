dff99fbed4c46710d5e65a0949867d2781d5bf11
Amit Dhingra <amit.dhingra@trantorinc.com>
2022-06-07 17:32:24 +0530

    port no changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const qs = require('querystring');
 const express = require('express');
 const routes = require('./api-routes');
 // const parser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
