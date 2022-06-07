c9954fec214b258ff3d9394c9b0ba930fa8fe1c2
Amit Dhingra <amit.dhingra@trantorinc.com>
2022-06-07 18:38:49 +0530

    mongo atlas connection

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 // Import Mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://madhavd:sbQwGR2BYIWtlPmU@cluster0.gq8vn.mongodb.net/test', { useNewUrlParser: true });
 var db = mongoose.connection;
